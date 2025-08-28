d8ef18012789ec1243fcb87f0c05010087e02712
whoisdomi <dcantu83@gmail.com>
2025-08-28 14:52:32 -0500

    Damp Factor slow/med/fast
    Update hyundaicanfd.py - damp factor per speed

--- selfdrive/car/hyundai/hyundaicanfd.py
+++ selfdrive/car/hyundai/hyundaicanfd.py
@@ -35,10 +35,18 @@ class CanBus(CanBusBase):
     return self._cam
 
 
-def create_steering_messages(packer, CP, CAN, enabled, lat_active, apply_steer):
+def create_steering_messages(packer, CP, CAN, enabled, lat_active, apply_steer, v_ego):
 
   ret = []
 
+  #damp_factor over 3 speeds
+  if v_ego <= 18: #40mph
+    damp_factor = 100
+  elif v_ego <= 26: #58mph
+    damp_factor = 150
+  else:
+    damp_factor = 200
+    
   values = {
     "LKA_MODE": 2,
     "LKA_ICON": 2 if enabled else 1 if lat_active else 0,  # HDA2
@@ -49,7 +57,7 @@ def create_steering_messages(packer, CP, CAN, enabled, lat_active, apply_steer):
     "HAS_LANE_SAFETY": 0,  # hide LKAS settings
     "NEW_SIGNAL_1": 0,
     "NEW_SIGNAL_2": 0,
-    "DAMP_FACTOR": 100,  # can potentially tuned for better perf [3, 200]
+    "DAMP_FACTOR": damp_factor,  # can potentially tuned for better perf [3, 200]
   }
 
   if CP.flags & HyundaiFlags.CANFD_HDA2:
