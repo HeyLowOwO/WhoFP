ac612434adf20abed94b8c6db4a9d045b95c52bb
whoisdomi <dcantu83@gmail.com>
2025-09-04 20:37:18 -0500

    Update latcontrol_torque.py low speed y calm

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -21,8 +21,11 @@ from openpilot.frogpilot.controls.lib.neural_network_feedforward import LOW_SPEE
 # move it at all, this is compensated for too.
 
 # STOCK VALUES
-LOW_SPEED_X = [0, 10, 20, 30]
-LOW_SPEED_Y = [15, 13, 10, 5]
+#LOW_SPEED_X = [0, 10, 20, 30]
+#LOW_SPEED_Y = [15, 13, 10, 5]
+
+LOW_SPEED_X = [0, 5, 8, 12, 18, 25, 30, 35]
+LOW_SPEED_Y = [12, 10, 6, 4, 2, 1, 1, 1]
 
 #LOW_SPEED_X = [0, 5, 10, 20, 30]
 #LOW_SPEED_Y = [22, 15, 13, 10, 5]
