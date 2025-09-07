af1b9efdfb42ac2ea2ef26d95114f4d65ba82d63
whoisdomi <dcantu83@gmail.com>
2025-09-07 13:01:41 -0500

    Update tinygrad_modeld.py fix

--- frogpilot/tinygrad_modeld/tinygrad_modeld.py
+++ frogpilot/tinygrad_modeld/tinygrad_modeld.py
@@ -394,7 +394,7 @@ def main(demo=False):
     is_rhd = sm["driverMonitoringState"].isRHD
     frame_id = sm["roadCameraState"].frameId
     v_ego = max(sm["carState"].vEgo, 0.)
-    lat_delay = sm["liveDelay"].lateralDelay + LAT_SMOOTH_SECONDS
+    lat_delay = sm["liveDelay"].lateralDelay + 0.25  # Use default smoothing for delay calculation
     lateral_control_params = np.array([v_ego, lat_delay], dtype=np.float32)
     if sm.updated["liveCalibration"] and sm.seen['roadCameraState'] and sm.seen['deviceState']:
       device_from_calib_euler = np.array(sm["liveCalibration"].rpyCalib, dtype=np.float32)
