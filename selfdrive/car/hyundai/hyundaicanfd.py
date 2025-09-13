9f672a007c66350d55986ed3da62ef05bd2a7447
whoisdomi <dcantu83@gmail.com>
2025-09-13 14:15:14 -0500

    Update hyundaicanfd.py damp 50/200

--- selfdrive/car/hyundai/hyundaicanfd.py
+++ selfdrive/car/hyundai/hyundaicanfd.py
@@ -41,9 +41,9 @@ def create_steering_messages(packer, CP, CAN, enabled, lat_active, apply_steer, 
 
   #damp_factor over 3 speeds
   if v_ego <= 18: #40mph
-    damp_factor = 100
+    damp_factor = 50
   elif v_ego <= 26: #58mph
-    damp_factor = 100
+    damp_factor = 200
   else:
     damp_factor = 200
     
